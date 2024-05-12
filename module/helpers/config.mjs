9b01873c93d0d766de8a3ca2c5792fa1e79cc7ae
Christopher Brown <chrisjosbrown@gmail.com>
2024-05-12 11:24:36 +0200

    init

---
+++ module/helpers/config.mjs
@@ -0,0 +1,25 @@
+export const FALLOUT_ZERO = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+FALLOUT_ZERO.abilities = {
+  str: 'FALLOUT_ZERO.Ability.Str.long',
+  per: 'FALLOUT_ZERO.Ability.Per.long',
+  end: 'FALLOUT_ZERO.Ability.End.long',
+  cha: 'FALLOUT_ZERO.Ability.Cha.long',
+  int: 'FALLOUT_ZERO.Ability.Int.long',
+  agi: 'FALLOUT_ZERO.Ability.Agi.long',
+  luc: 'FALLOUT_ZERO.Ability.Luc.long',
+};
+
+FALLOUT_ZERO.abilityAbbreviations = {
+  str: 'FALLOUT_ZERO.Ability.Str.abbr',
+  per: 'FALLOUT_ZERO.Ability.Per.abbr',
+  end: 'FALLOUT_ZERO.Ability.End.abbr',
+  cha: 'FALLOUT_ZERO.Ability.Cha.abbr',
+  int: 'FALLOUT_ZERO.Ability.Int.abbr',
+  agi: 'FALLOUT_ZERO.Ability.Agi.abbr',
+  luc: 'FALLOUT_ZERO.Ability.Luc.abbr',
+};
